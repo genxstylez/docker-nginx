acdfb4013d1b9f03a8684ae2bfc82299a39b966d
Sam Liu <genxstylez@gmail.com>
2016-07-15 03:05:24 +0800

    add server_name as env var

--- 1.10.0/docker-entrypoint.sh
+++ 1.10.0/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" = 'nginx' ]; then
     echo Starting nginx.
-    envsubst '$$UPSTREAM_HOSTNAME $$UPSTREAM_PORT' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+    envsubst '$$UPSTREAM_HOSTNAME $$UPSTREAM_PORT $$SERVER_NAME' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 fi
 
 exec "$@"
