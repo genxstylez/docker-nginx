ea599c4dbf90e5ca37dc102cfeb00a193fc1c6a7
Sam Liu <genxstylez@gmail.com>
2016-08-31 12:15:26 +0800

    clean repo

--- 1.10.0/docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ "$1" = 'nginx' ]; then
-    echo Starting nginx.
-    envsubst '$$UPSTREAM_HOSTNAME $$UPSTREAM_PORT $$SERVER_NAME' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
-fi
-
-exec "$@"
